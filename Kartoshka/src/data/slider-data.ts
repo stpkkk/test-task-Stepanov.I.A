4407091105140d682f1c24bdd674704d3f441ed7
Igor Stepanov <stipyk1309@gmail.com>
2023-05-14 22:57:21 +0300

    test6: package.json fix

--- Kartoshka/src/data/slider-data.ts
+++ Kartoshka/src/data/slider-data.ts
@@ -1,4 +1,10 @@
-export const getSliderData = [
+interface ISlider {
+  image: string;
+  title: string;
+  price: number;
+}
+
+export const getSliderData: ISlider[] = [
   { image: 'potatoTable', title: 'Столовый картофель', price: 142 },
   { image: 'potatoCountry', title: 'Деревенский картофель', price: 129 },
   { image: 'potatoHomemade', title: 'Домашний картофель', price: 133 },
